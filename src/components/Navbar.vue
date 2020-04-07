018810c914cdd59aef9ee8bc68cf790fdb92b19f
xirpl2 <fariz_akbar27rpl@student.smktelkom-mlg.sch.id>
2020-04-07 17:42:07 +0700

    0.3

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+	<template>
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="#/">Ol<span>Ser</span></a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="#/" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="#/" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="#/" class="nav-link">Services</a></li>
+	          <li class="nav-item"><a href="#/" class="nav-link">Pricing</a></li>
+	          <li class="nav-item"><a href="#/" class="nav-link">Cars</a></li>
+	          <li class="nav-item"><a href="#/" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><router-link to="/login" class="nav-link">Login / Register</router-link></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+	</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Navbar from '../components/Navbar.vue'
+import Footer from '../components/Footer.vue'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
+
 
 Vue.use(VueRouter)
 
@@ -8,16 +13,18 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    components: {default: Home, header: Navbar, footer: Footer}
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'login',
+    components: {default: Login}
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    components: {default: Signup}
+  },
 ]
 
 const router = new VueRouter({
